ac12cbaa9761426af250c3f657df2e44380c91f6
César Grisel <griselcesar@gmail.com>
2023-10-26 12:04:20 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import { connect } from 'mongoose'
+import { config } from 'dotenv'
+
+import app from './Server/index.js'
+
+config()
+
+connect(process.env.URI_DB)
+.then(()=>{
+  console.log('db connect successful 🚀')
+  app.listen(process.env.PORT,()=>{
+    console.log('Server listen on port: '+process.env.PORT)
+  })
+})
